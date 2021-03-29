ac7726061c6a54d7bc7d6c302c601931c16c32f8
Jesse Phipps <jessefphipps@gmail.com>
2021-03-28 19:18:00 -0500

    Added the output function

--- coding_file.cpp
+++ coding_file.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
+
+
+void output (string input){
+    cout << "Here's the input: " << input << endl;
+}
 
 int main (){
 
 
-    
     return 1;
 }
